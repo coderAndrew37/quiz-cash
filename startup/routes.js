4eba5fdfde75b178b48d22c31e7ab138ff752717
coderAndrew37 <omolloandrew37@gmail.com>
2024-11-03 11:07:27 +0300

    removing the duplicate user API

--- startup/routes.js
+++ startup/routes.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const quizzes = require("../routes/quizzes");
-const users = require("../routes/users"); // Import the users route
 const auth = require("../routes/auth");
 
 module.exports = function (app) {
@@ -8,6 +7,5 @@ module.exports = function (app) {
   app.use("/api/quizzes", quizzes);
 
   // Route for user-related actions (register, login)
-  app.use("/api/users", users);
   app.use("/api/auth", auth);
 };
