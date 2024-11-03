99d215c16c9c60b93d054cbf95d91561863612ee
coderAndrew37 <omolloandrew37@gmail.com>
2024-11-03 11:17:40 +0300

    ..

--- startup/routes.js
+++ startup/routes.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const quizzes = require("../routes/quizzes");
-const auth = require("../routes/auth");
+//const auth = require("../routes/auth");
 
 module.exports = function (app) {
   // Route for quiz-related actions
   app.use("/api/quizzes", quizzes);
 
   // Route for user-related actions (register, login)
-  app.use("/api/auth", auth);
+  //app.use("/api/auth", auth);
 };
